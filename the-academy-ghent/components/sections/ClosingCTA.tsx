5dcdf096dd163ffc9245e3b01dff95cd61be420c
TheAcademyGhent <info@theacademyghent.com>
2026-08-14 19:04:23 +0200

    Update button href to use CTA_HREF variable

--- the-academy-ghent/components/sections/ClosingCTA.tsx
+++ the-academy-ghent/components/sections/ClosingCTA.tsx
@@ -30,7 +30,7 @@ export default function ClosingCTA() {
           </p>
         </Reveal>
         <Reveal delay={0.2}>
-          <Button href="/schrijf-je-in" variant="primary" size="lg">
+          <Button href={CTA_HREF} variant="primary" size="lg">
             Boek een gratis proefles
           </Button>
         </Reveal>
