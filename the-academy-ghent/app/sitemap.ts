f4f19edeacf9c56675f62d9c8bc5c98683a4e931
TheAcademyGhent <info@theacademyghent.com>
2026-08-18 11:22:27 +0200

    Add privacy policy route to sitemap

--- the-academy-ghent/app/sitemap.ts
+++ the-academy-ghent/app/sitemap.ts
@@ -10,6 +10,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "/lidmaatschappen",
     "/contact",
     "/schrijf-je-in",
+    "/privacybeleid",
     "/algemene-voorwaarden",
   ];
 
